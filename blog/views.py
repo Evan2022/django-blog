391b02c2d0f17aa668e6323624d20a34171fe547
Evan2022 <evan201198@hotmail.com>
2022-10-08 14:03:31 +0000

    Add templates and accounts folders and html files

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.object.filter(status=1).order_by('created_on') #filter by published posts in order of the date they were created on
+    template_name = 'index.html' 
+    paginate_by = 6 #The max amount of posts visible on the page, Django will automatically add page navigation
+
+
